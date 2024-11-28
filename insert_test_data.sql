ccf25c00225c4d5c9130d93ebbaae83af06643b8
naoya_nara <naoya706oxford@gmail.com>
2024-11-28 10:11:44 +0000

    implement news list feature

---
+++ insert_test_data.sql
@@ -0,0 +1,4 @@
+USE newshub;
+
+INSERT INTO news (author, title, description, category) VALUES ('author1','test title','test description', 'test');
+INSERT INTO news (author, title, description, category) VALUES ('author2','test title2','test description2', 'test2');
